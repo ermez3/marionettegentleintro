d504430226b6e167135badd546492bfabe601c9d
ermez3 <ermez3@gmail.com>
2013-09-09 02:32:41 -0500

    Until Page 50
    Code until page 50 , with slightly changes in order to use handlebars
    precompiled templates instead of underscore.
    Last Chapter Builing CompositeView. Similar to CollectionView Backbone
    but more powerfull to handle wrappers in templates.

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,13 +1,16 @@
 ContactManager.module('ContactsApp.List', function(List,ContactManager,Backbone,Marionette, $ , _) {
   
   List.Contact = Marionette.ItemView.extend({
-		tagName: "li",
-		template: "#contact-list-item"
+		tagName: "tr",
+		template: Handlebars.templates.contactlistitem
 	});
 
-  List.Contacts = Marionette.CollectionView.extend({
-		tagName: "ul",
-		itemView: List.Contact
+  List.Contacts = Marionette.CompositeView.extend({
+		tagName: "table",
+		className: "table table-hover",
+		template: Handlebars.templates.contactlist,
+		itemView: List.Contact,
+		itemViewContainer: "tbody"
 	});
 
 });
---
+++ assets/js/templates.js
@@ -0,0 +1,28 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['contactlist'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<thead>\r\n <tr><th>First Name</th><th>Last Name</th></tr>\r\n </thead>\r\n <tbody>\r\n</tbody>";
+  });
+templates['contactlistitem'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<td>";
+  if (stack1 = helpers.firstName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.firstName; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</td>  <td>";
+  if (stack1 = helpers.lastName) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.lastName; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</td>\r\n";
+  return buffer;
+  });
+})();
