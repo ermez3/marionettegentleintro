8caad1885946ee0d15aab384541512215f213f58
ermez3 <ermez3@gmail.com>
2013-09-09 13:34:08 -0500

    Page 76
    Routing Helpers

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,21 @@ ContactManager.addRegions({
 	mainRegion: "#main-region"
 });
 
+ContactManager.navigate = function(route,options){
+	options||(options={});
+	Backbone.history.navigate(route,options);
+};
+
+ContactManager.getCurrentRoute = function(){
+	return Backbone.history.fragment;
+};
+
 ContactManager.on("initialize:after",function(){
-		if(Backbone.history){
-			Backbone.history.start();
+	if(Backbone.history){
+		Backbone.history.start();
+		if(this.getCurrentRoute() === ""){
+			this.navigate("contacts");
+			ContactManager.ContactsApp.List.Controller.listContacts();
 		}
-	});
+	}	
+});
